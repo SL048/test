99d7beccb7462f0cc7ef304a4dd356f914ba5ca1
apac94 <apac94@mail.ru>
2021-04-13 18:02:50 +0500

    Add -ticket controller -ticket view -service

---
+++ Presentation/TaskManager.Web/Controllers/TicketController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskManager.Web.Controllers
+{
+   public class TicketController : Controller
+   {
+      private readonly ITicketRepository _ticketRepository;
+
+      public TicketController(ITicketRepository ticketRepository)
+         => _ticketRepository = ticketRepository;
+
+      public IActionResult Index()
+      {
+         var tickets = _ticketRepository.GetAllTickets();
+         return View(tickets);
+      }
+   }
+}
